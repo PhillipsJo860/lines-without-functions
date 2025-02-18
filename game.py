5883d36a8b1c35201beb155f8272f9f4bce2a087
SoggyCereal <PhillipsJo860@northwested.org>
2025-02-18 10:39:21 -0500

    made_change

--- game.py
+++ game.py
@@ -23,13 +23,38 @@ def main():
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.WHITE)
-        pygame.display.flip()
+        screen.fill(config.COLOR_WHITE)
+        
 
+        start_coordinate1 = (50, 15)
+        start_coordinate2 = (800, 450)
+        start_coordinate3 = (340, 100)
+        start_coordinate4 = (205, 313)
+        start_coordinate5 = (234,545)
+        start_coordinate6 = (500, 600)
+
+        end_coordinate1 = (623, 300)
+        end_coordinate2 = (613, 300)
+        end_coordinate3 = (123, 300)
+        end_coordinate4 = (665, 300)
+        end_coordinate5 = (432, 300)
+        end_coordinate6 = (500, 300)
+        line_thickness = (5)
+
+        pygame.draw.line(screen, config.COLOR_RED, start_coordinate1, end_coordinate1, line_thickness)
+        pygame.draw.line(screen, config.COLOR_BLACK, start_coordinate2, end_coordinate2, line_thickness)
+        pygame.draw.line(screen, config.COLOR_GREEN, start_coordinate3, end_coordinate3, line_thickness)
+        pygame.draw.line(screen, config.COLOR_BLUE, start_coordinate4, end_coordinate4, line_thickness)
+        pygame.draw.line(screen, config.COLOR_CYAN, start_coordinate5, end_coordinate5, line_thickness)
+        pygame.draw.line(screen, config.COLOR_SANDYBROWN, start_coordinate6, end_coordinate6, line_thickness)
+
+        pygame.display.flip()
         clock.tick(config.FPS)
     
     pygame.quit()
     sys.exit()
+
+
 
 if __name__ == '__main__':
     main()
